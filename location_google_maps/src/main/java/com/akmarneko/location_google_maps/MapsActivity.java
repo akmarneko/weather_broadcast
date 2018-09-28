540b79d728da543c520a02cb6fbfac2fadbfe912
akmarneko <akmarneko@gmail.com>
2018-09-28 16:14:16 +0800

    add current location

--- location_google_maps/src/main/java/com/akmarneko/location_google_maps/MapsActivity.java
+++ location_google_maps/src/main/java/com/akmarneko/location_google_maps/MapsActivity.java
@@ -1,5 +1,8 @@
 package com.akmarneko.location_google_maps;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -38,9 +41,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        //to get current location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            return;
+        }else{
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //public void onRequestPermissionsResult(int requestCode, String[] permissions,int[] grantResults)
+            //to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+        }
+
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //default distance view
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //allow to zoom 1-21 small to bigger
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 16));
+
+
+
     }
 }
